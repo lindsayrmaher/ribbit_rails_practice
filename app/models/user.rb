c3772d34dca18e5a405b709661ddd9241bfe8c65
Lindsay Maher <lindsaymah3r@gmail.com>
2017-08-19 17:48:33 -0500

    created user relationships structure

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  has_many :follower_relationships, classname: "Relationship", foreign_key: "followed_id"
+  has_many :followed_relationships, classname: "Relationship", foreign_key: "follower_id"
+  has_many :followers, through: :follower_relationships
+  has_many :followeds, through: :followed_relationships
   has_many :ribbits
   has_secure_password
   before_validation :prep_email
@@ -7,6 +11,14 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :username, uniqueness: true, presence: true
   validates :email, uniqueness: true, presence: true, format: { with: /\A[\w.+-]+@([\w]+.)+\w+\z/ }
+
+  def following? user
+      self.followeds.include? user
+  end
+   
+  def follow user
+      Relationship.create follower_id: self.id, followed_id: user.id
+  end
 
   private
 
