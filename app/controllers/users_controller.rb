207081bdf663642541d0f0119788704ba176a17a
Lindsay Maher <lindsaymah3r@gmail.com>
2017-08-19 15:50:16 -0500

    created log in capability for returning users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
     @user = User.new(allowed_params)
 
     if @user.save
+      session[:user_id] = @user.id
       redirect_to @user, notice: "Thank you for signing up for Ribbit!"
     else
       render 'new'
